b51a0334ee5677a291f03f0fbcb4a2bfe40b012c
Bhavesh Gadag <bhaveshgadag46@gmail.com>
2026-08-16 00:49:36 +0530

    Add endpoints:partials:css

--- 26 Capstone/index.js
+++ 26 Capstone/index.js
@@ -1,9 +1,17 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get("/", (req, res) => {
+    res.redirect("/home");
+});
+
+app.get("/home", (req, res) => {
     res.render("home.ejs");
 });
 
